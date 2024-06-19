5d4c276f0b69ef797d394b04fac6915c8c78d828
LilMasterOogway <110360959+LilMasterOogway@users.noreply.github.com>
2024-06-19 12:24:57 +1200

    Title and settings

--- MyGame/CharacterBody3D.gd
+++ MyGame/CharacterBody3D.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 
-@export var speed = 5
+@export var speed = 2
+@export var sprint_speed = 4
+@export var current_speed = speed
 @export var mouse_sensitivity = 0.005
 @onready var camera = $Camera3D
 @onready var ray = $Camera3D/RayCast3D
@@ -17,14 +19,22 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var move_dir = transform.basis * Vector3(input_dir.x, 0, input_dir.y).normalized()
 	if input_dir:
-		velocity.x = move_dir.x * speed
-		velocity.z = move_dir.z * speed
+		velocity.x = move_dir.x * current_speed
+		velocity.z = move_dir.z * current_speed
 	else:
 		velocity.x = 0
 		velocity.z = 0
-		
-	if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
+	
+	if Input.is_action_pressed("sprint"):
+		current_speed = sprint_speed
+	else:
+		current_speed = speed
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = jump_speed
+		
+	if Input.is_action_pressed("pause"):
+		get_tree().change_scene_to_file("res://scenes/title.tscn")
 		
 	move_and_slide()
 	
@@ -45,3 +55,5 @@ func _input(event):
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		camera.rotate_x(-event.relative.y * mouse_sensitivity)
 		camera.rotation.x = clamp(camera.rotation.x, -1,1)
+	
+
--- MyGame/scenes/title.gd
+++ MyGame/scenes/title.gd
@@ -1,7 +1,9 @@
 extends CanvasLayer
-
+@export var world : PackedScene
+@export var settings : PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	pass # Replace with function body.
 
 
@@ -11,6 +13,16 @@ func _process(delta):
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://World.tscn")
+	get_tree().change_scene_to_packed(world)
 	pass # Replace with function body.
 	
+
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_packed(settings)
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
